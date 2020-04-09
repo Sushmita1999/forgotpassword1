41c9da815318a89fffacb8628dd13e171b284356
Sushmita1999 <sushmitashetty2017@gmail.com>
2020-04-09 23:57:28 +0530

    First commit

---
+++ routes/forgetpassword.js
@@ -0,0 +1,66 @@
+const express=require('express');
+const router=express.Router();
+//require("dotenv").config()
+const crypto=require("crypto")
+
+const nodemailer=require("nodemailer")
+const forgotpassword=require("../models/forgetpasswordmodel");
+
+router.post("/forgot",async(req,res)=>
+{
+    try{
+        if(req.body.email===" "){
+            console.log("No email found");
+            res.status(400).send("Email required");
+        }
+        let{ email }=req.body;
+        console.log(email);
+        console.log("email destructured")
+        let emailid= await forgotpassword.emailcheck(email);
+        console.log(emailid);
+        if(emailid===null){
+            console.log("email not in database");
+            res.status(403).send("email not in database");
+        }else{
+            console.log("email checked");
+            const token=crypto.randomBytes(20).toString("hex");
+            console.log("go to update");
+            let updated= await forgotpassword.emailupdate(email,token);
+            console.log("updation done properly");
+            const transporter=nodemailer.createTransport({
+                host:"smtp.gmail.com",
+                port:587,
+                secure:false,
+                auth:{
+                    user:'vhgsvcgh@gmail.com',
+                    pass:'djncjdbdhbc'
+                },
+                tls:{rejectUnauthorized:false
+                }
+            })
+            console.log("transporter is also done");
+            const mailoptions={
+                from:"vhgsvcgh@gmail.com",
+                to:`${emailid.email}`,
+                subject:"Link to Reset Password",
+                text:`http://localhost:3000/reset/${token}\n\n`,
+                
+            }
+            console.log("mailoptions done properly");
+            transporter.sendMail(mailoptions,(err,res)=>{
+                if(err){
+                    console.log("there was a error",err)
+                }else{
+                    console.log("Here is a response",res);
+                    res.status(200).json("recovery mail sent");
+                }
+            })
+            console.log("mail sent !!!!!")
+
+        }
+
+    }catch(error){
+        console.log(error);
+    }
+});
+module.exports=router;
